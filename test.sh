ec3298d27f26c2ac7383f4d36356fd90fd7f0ccf
Steven Dong <sdong488@gmail.com>
2025-04-22 18:19:10 +0200

    spelling mistake

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
 source $HOME/venv_test/bin/activate
 
-cp -r $HOME/bacheleor-thesis "$TMPDIR"
+cp -r $HOME/bachelor-thesis "$TMPDIR"
 cd "$TMPDIR"/bachelor-thesis/PAG-main
 
 bash full_scripts/full_lexical_ripor_evaluate.sh
