fd127e55a339b2e79792025c44240e6d96e51a18
Steven Dong <sdong488@gmail.com>
2025-05-12 00:20:53 +0200

    update job

--- scripts/train_indexer.sh
+++ scripts/train_indexer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_finqa_indexer
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-node=18
 #SBATCH --gpus-per-node=4
 #SBATCH --partition=gpu_a100
