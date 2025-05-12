38d449a3517f03e9ca026d3f528df17e27dad090
Steven Dong <sdong488@gmail.com>
2025-05-12 21:34:19 +0200

    add logging and fix copy

--- scripts/init_job.sh
+++ scripts/init_job.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
+echo "Script started at: $(date '+%Y-%m-%d %H:%M:%S')"
+
 # Load modules
 module purge
 module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
+echo "Modules loaded at: $(date '+%Y-%m-%d %H:%M:%S')"
+
 # Copy virtualenv from home to compute and activate
 # Prevents reading packages on home during runtime
 cp -r $HOME/venv_glen "$TMPDIR"/venv_glen
 source "$TMPDIR"/venv_glen/bin/activate
 
+echo "Environment loaded at: $(date '+%Y-%m-%d %H:%M:%S')"
+
 # Copy data and code from home to compute
 # Better I/O and preserve file structure
 # Ignore unused files
 
-# 1. Make sure dest exists
 mkdir -p "$TMPDIR"/bachelor-thesis
 
-# 2. From your home dir, find all files/dirs except the ones to skip,
-#    and copy them into $TMPDIR/bachelor-thesis, recreating their paths.
-cd "$HOME"
-find bachelor-thesis \
-     \( -path 'bachelor-thesis/old' \
-     -o -path 'bachelor-thesis/FinQA-main' \
-     -o -path 'bachelor-thesis/GLEN-main' \) -prune \
-     -o -print0 \
-| xargs -0 -I{} cp --parents -R {} "$TMPDIR"/bachelor-thesis
+cp -r $HOME/bachelor-thesis/code "$TMPDIR"/bachelor-thesis
+cp -r $HOME/bachelor-thesis/data "$TMPDIR"/bachelor-thesis
+cp -r $HOME/bachelor-thesis/models "$TMPDIR"/bachelor-thesis
+cp -r $HOME/bachelor-thesis/scripts "$TMPDIR"/bachelor-thesis
+
+echo "Initialization completed at: $(date '+%Y-%m-%d %H:%M:%S')"
